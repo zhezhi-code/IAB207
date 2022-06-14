5bccb43cd1d96b35c24010dbf7d5042197603ae4
YU PENG Tang <74084289+zhezhi-code@users.noreply.github.com>
2022-06-14 15:24:23 +1000

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from ensurepip import bootstrap
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 db = SQLAlchemy()
 
 def create_app():
@@ -13,6 +14,15 @@ def create_app():
 
     app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///Assessment222.sqlite'
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view='auth.login'
+    login_manager.init_app(app)
+
+    from .models import User  # importing here to avoid circular references
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
 
     #add Blueprints
